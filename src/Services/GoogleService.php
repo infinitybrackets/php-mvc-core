ddaa16d1581e9e25419ef3ef0631c6cb6e1b71e5
John Vincent Bonza <jayveeinfinity@gmail.com>
2022-09-22 16:19:50 +0800

    Add GoogleService checking

--- src/Services/GoogleService.php
+++ src/Services/GoogleService.php
@@ -9,19 +9,21 @@ class GoogleService
     public \Google\Client $client;
 
     public function __construct() {
-        // Initialize Google Client Credentials
-        $this->client = new \Google\Client();
-        $google = Application::$app->config->auth->google;
-        
-        $this->client->setClientId($google->id);
-        $this->client->setClientSecret($google->secret);
-        $this->client->setRedirectUri($google->redirect);
+        if(!empty(Application::$app->config->auth->defaults->auth)) {
+            // Initialize Google Client Credentials
+            $this->client = new \Google\Client();
+            $google = Application::$app->config->auth->google;
+            
+            $this->client->setClientId($google->id);
+            $this->client->setClientSecret($google->secret);
+            $this->client->setRedirectUri($google->redirect);
 
-        // Add Google Client Scope
-        $this->client->addScope('profile');
-        $this->client->addScope('email');
+            // Add Google Client Scope
+            $this->client->addScope('profile');
+            $this->client->addScope('email');
 
-        // Create Google Login URL
-        $google->url = $this->client->createAuthUrl();
+            // Create Google Login URL
+            $google->url = $this->client->createAuthUrl();
+        }
     }
 }
